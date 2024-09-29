46a961a61b4e296607a28ab9e828c4484a442789
rmdocherty <rmdocherty2@gmail.com>
2024-09-29 17:16:15 +0100

    code to apply (semi) trained model to new data - needs some cruft as have messed up my train inputs somewhat and need to recompute similar using featup

--- apply.py
+++ apply.py
@@ -1,39 +1,90 @@
 import torch
+import torch.nn.functional as F
+import torchvision.transforms as T
+from torchvision.transforms import functional as TF
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import normalize
 
-from utils import to_numpy, do_2D_pca, add_flash_attention, load_image, closest_crop
+from utils import (
+    to_numpy,
+    do_2D_pca,
+    add_flash_attention,
+    load_image,
+    closest_crop,
+    resize_crop,
+    get_lr_feats,
+)
 from model import Combined
 
 
 DEVICE = "cuda:0"
 dv2 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
 dv2 = add_flash_attention(dv2)
-dv2 = dv2.eval().half().to(DEVICE)
+dv2 = dv2.eval().to(DEVICE).half()
 
 
-upsampler = torch.load("upsample.pth")
+upsampler = torch.load("e460.pth")
 """
 Combined(
     14, n_ch_img=3, n_ch_in=128, n_ch_downsample=64, k_up=3, feat_weight=0.25
 )
 """
-upsampler = upsampler.eval().half().to(DEVICE)
+upsampler: Combined = upsampler.eval().to(DEVICE)
 
-path = "data/compare/3.jpg"
-_img = Image.open(path)
+path = "data/compare/default_image.jpg"
+L = 224 * 3
+_img = Image.open(path).convert("RGB").resize((L, L))
 _h, _w = _img.height, _img.width
-tr = closest_crop(_h, _w)
+# tr = closest_crop(_h, _w)
+norm = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+tr = T.Compose(
+    [
+        T.Resize(L),
+        T.CenterCrop((L, L)),
+        T.ToTensor(),
+        norm,
+    ]
+)
+
+
 img, original = load_image(path, tr)
+
+inp_img = (
+    TF.normalize(
+        TF.pil_to_tensor(_img).to(torch.float32),
+        [0.485, 0.456, 0.406],
+        [0.229, 0.224, 0.225],
+    )
+    .unsqueeze(0)
+    .to(DEVICE)
+)
 
 _, _, h, w = img.shape
 
+HALF = True
+if HALF:
+    dv2 = dv2.half()
+    img = img.half()
 
-n_patch_w: int = 1 + (w - 14) // 14
-n_patch_h: int = 1 + (h - 14) // 14
-feats_dict: dict = dv2.forward_features(img)  # type: ignore
-feats = feats_dict["x_norm_patchtokens"]
-b, _, c = feats.shape
-feats = feats.reshape(b, c, n_patch_h, n_patch_w)
-print(feats.shape)
-reduced = do_2D_pca()
-print(reduced.shape)
+print(img.shape)
+original = original.convert("RGB")
+reduced_tensor = get_lr_feats(
+    dv2, img
+)  # torch.tensor(reduced_np).permute((-1, 0, 1)).unsqueeze(0)
+
+data = torch.load("data/imagenet_reduced/val/0.pth")["lr_feats"]
+
+
+reduced_tensor = reduced_tensor.to(DEVICE).to(torch.float32)
+reduced_tensor = F.normalize(reduced_tensor, p=1, dim=1)
+hr_feats = upsampler(inp_img, reduced_tensor)
+hr_feats_np = to_numpy(hr_feats)
+# hr_feats_np = np.nan_to_num(hr_feats_np, nan=0)
+reduced_hr = do_2D_pca(hr_feats_np, 3, post_norm="minmax")
+plt.imsave(
+    "test.png",
+    reduced_hr,
+)
+# print([d.shape for d in downsamples])
