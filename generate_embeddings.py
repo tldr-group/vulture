8f9e05cea0565cee540542165d8c4f4a8ccfb794
rmdocherty <rmdocherty2@gmail.com>
2024-09-30 10:21:35 +0100

    added code to gen dv2 embeddings for images

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -1,11 +1,18 @@
 import torch
+import torchvision.transforms as T
 import argparse
 import hydra
 from omegaconf import OmegaConf
+from os import listdir
+from PIL import Image
 from featup.train_implicit_upsampler import my_app
 
 torch.manual_seed(1001)
 
+DEVICE = "cuda:0"
+dv2 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
+# dv2 = add_flash_attention(dv2)
+dv2 = dv2.eval().to(DEVICE)  # .half()
 
 with open("FeatUp/featup/configs/implicit_upsampler.yaml") as f:
     basic_conf_file = f.read()
@@ -21,6 +28,39 @@ def start_featup(split_n: int) -> None:
     my_app(new_config)
 
 
+norm = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+tr = T.Compose(
+    [
+        T.ToTensor(),
+        norm,
+    ]
+)
+
+
+def gen_original_dv2_embeds(model: torch.nn.Module, folder: str) -> None:
+    data_path = "data/imagenet_reduced"
+    for i, fname in enumerate(listdir(f"{data_path}/{folder}")):
+        img_idx = int(fname.split(".")[0])
+        img_fname = f"{data_path}/splits/{img_idx % 10}/{img_idx}.png"
+        img = Image.open(img_fname).convert("RGB")
+
+        tensor: torch.Tensor = tr(img).unsqueeze(0).cuda()
+        b, _, h, w = tensor.shape
+        nt_w: int = 1 + (w - 14) // 14
+        nt_h: int = 1 + (h - 14) // 14
+        feat_dict: dict = model.forward_features(tensor)
+        feats = feat_dict["x_norm_patchtokens"]
+        _, nt, c = feats.shape
+        feats = feats.permute((0, 2, 1))
+        feats = feats.reshape((b, c, nt_h, nt_w)).detach().cpu()
+
+        data = torch.load(f"{data_path}/{folder}/{fname}", weights_only=True)
+        data["dv2_lr_feats"] = feats
+        torch.save(data, f"{data_path}/{folder}/{fname}")
+
+        print(i)
+
+
 parser = argparse.ArgumentParser(description="get split")
 parser.add_argument(
     "--n",
@@ -33,3 +73,4 @@ if __name__ == "__main__":
     # so far have done 0 and 2
     # need to do 3, 4, 5, 6, 7, 8, 9
     start_featup(args.n)
+    # gen_original_dv2_embeds(dv2, "data")
