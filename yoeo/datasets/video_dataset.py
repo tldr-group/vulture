f6faa4d2e667e4f5d3aef7d193ab3a97381fc9c1
rmdocherty <rmdocherty2@gmail.com>
2025-01-27 09:54:49 +0000

    added fn to get features from image batch

--- yoeo/datasets/video_dataset.py
+++ yoeo/datasets/video_dataset.py
@@ -1,12 +1,14 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from torchvision.transforms import functional as TF  # type: ignore
 from json import load
 import numpy as np
 from PIL import Image
 
-from yoeo.utils import Experiment, paired_frames_vis
+from yoeo.utils import Experiment, paired_frames_vis, expriment_from_json
 
+from timm.models import VisionTransformer
 from typing import Literal
 
 
@@ -15,7 +17,7 @@ class VideoDataset(Dataset):
         self,
         root_dir: str,
         which: Literal["train", "val"],
-        expr: Experiment | None,
+        expr: Experiment,
     ) -> None:
         super().__init__()
 
@@ -66,13 +68,51 @@ class VideoDataset(Dataset):
             cropped = TF.center_crop(tensor, [518, 518])
             normed = TF.normalize(cropped, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             imgs.append(normed)
-        return (imgs[0], imgs[1])
+        imgs_0, imgs_1 = imgs
+        return (imgs_0, imgs_1)
+
+    @torch.no_grad()
+    def get_features_of_batches(
+        self, model: VisionTransformer, imgs_0: torch.Tensor, imgs_1: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        _, _, h, w = img_0.shape
+        n_patch_h, n_patch_w = h // self.expr.patch_size, w // self.expr.patch_size
+
+        input_feat_dict: dict[str, torch.Tensor] = model.forward_features(imgs_0)  # type: ignore
+        target_feat_dict: dict[str, torch.Tensor] = model.forward_features(imgs_1)  # type: ignore
+
+        flat_input_feats = input_feat_dict["x_norm_patchtokens"]
+        flat_target_feats = input_feat_dict["x_norm_patchtokens"]
+        b, n_t, c = flat_input_feats.shape
+
+        if self.expr.norm:
+            flat_input_feats = F.normalize(flat_input_feats, p=1, dim=1)
+            flat_target_feats = F.normalize(flat_input_feats, p=1, dim=1)
+
+        input_feats = flat_input_feats.reshape((b, n_patch_h, n_patch_w, c))
+        target_feats = flat_target_feats.reshape((b, n_patch_h, n_patch_w, c))
+
+        return (input_feats, target_feats)
 
 
+DEVICE = "cuda:1"
 if __name__ == "__main__":
-    ds = VideoDataset("data/lvos", "train", None)
+
+    dv2 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14_reg")
+    dv2 = dv2.eval().to(DEVICE)
+
+    expr = expriment_from_json("yoeo/models/configs/simple_no_trs_dv2.json")
+    ds = VideoDataset("data/lvos", "train", expr)
     dl = DataLoader(ds, 20, True)
     img_0, img_1 = next(iter(dl))
+    img_0 = img_0.to(DEVICE)
+    img_1 = img_1.to(DEVICE)
+
+    inp_feats, outp_feats = ds.get_features_of_batches(dv2, img_0, img_1)
+    print(inp_feats.shape)
+
+    img_0 = img_0.to("cpu")
+    img_1 = img_1.to("cpu")
 
     n_samples = 8
     paired_frames_vis(img_0[:n_samples], img_1[:n_samples], "out.png")
