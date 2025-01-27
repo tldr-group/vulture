6a46d73ae5bf35af95831ee54deb957696b87cd5
rmdocherty <rmdocherty2@gmail.com>
2025-01-27 12:57:46 +0000

    updated install script

--- install/download_prep_lvos.sh
+++ install/download_prep_lvos.sh
@@ -7,7 +7,9 @@ rm -f data/lvos/train/train_meta.json data/lvos/train/train_expression_meta.json
 rm -f data/lvos.zip
 
 gdown https://drive.google.com/file/d/17Hwc__6i2rpF5e2s5OPqoywNxG5bzlcO/view?usp=share_link -O data/lvos_val.zip --fuzzy
-unzip data/lvos.zip -d data/lvos # unzips to data/lvos/val
+unzip data/lvos_val.zip -d data/lvos # unzips to data/lvos/val
+mv data/lvos/valid data/lvos/val
 rm -rf data/lvos/val/Annotations
+rm -rf data/lvos/val/Scribbles
 rm -f data/lvos/val/train_meta.json data/lvos/val/train_expression_meta.json
 rm -f data/lvos_val.zip
