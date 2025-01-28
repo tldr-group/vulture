41a8838524eb140e307ef20aaf972604e4441d7f
rmdocherty <rmdocherty2@gmail.com>
2025-01-28 09:53:53 +0000

    updated for val

--- install/download_prep_mose.sh
+++ install/download_prep_mose.sh
@@ -11,7 +11,5 @@ rm -f data/mose.tar.gz
 gdown https://drive.google.com/file/d/1yFoacQ0i3J5q6LmnTVVNTTgGocuPB_hR/view?usp=drive_link -O data/mose_val.tar.gz --fuzzy
 tar -xzf data/mose_val.tar.gz -C data/mose
 mv data/mose/valid data/mose/val
-rm -rf data/lvos/val/Annotations
-rm -rf data/lvos/val/Scribbles
-rm -f data/lvos/val/train_meta.json data/lvos/val/train_expression_meta.json
-rm -f data/lvos_val.zip
+rm -rf data/mose/val/Annotations
+rm -f data/mose_val.zip
