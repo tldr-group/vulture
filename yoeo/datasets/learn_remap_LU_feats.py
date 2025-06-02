5701cd3a93d4b6be62909fd8d68c8a6ad6b1746f
rmdocherty <rmdocherty2@gmail.com>
2025-06-02 14:53:10 +0100

    add conv remapping

--- yoeo/datasets/learn_remap_LU_feats.py
+++ yoeo/datasets/learn_remap_LU_feats.py
@@ -1,92 +1,48 @@
-from typing import Mapping
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
-from torch.utils.data import TensorDataset, DataLoader
 
 from PIL import Image
-from os import listdir
-import numpy as np
 
 from yoeo.utils import do_2D_pca
 import matplotlib.pyplot as plt
 
 
-from typing import Mapping, Any
-
-torch.random.manual_seed(0)
-np.random.seed(0)
-DEVICE = "cuda:0"
-
-
-def get_train_data(lr_feats: torch.Tensor, hr_feats: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-    _, c, n_t_h, n_t_w = lr_feats.shape
-    # _, c, h, w = hr_feats.shape
-
-    # lr_hr_feats = F.interpolate(hr_feats, (n_t_h, n_t_w), mode="bilinear")
-    lr_hr_feats = F.avg_pool2d(hr_feats, (14, 14), 14)
-
-    lr_flat: torch.Tensor = lr_feats.reshape((c, -1)).permute((1, 0))
-    lr_hr_flat: torch.Tensor = lr_hr_feats.reshape((c, -1)).permute((1, 0))
-
-    return (lr_hr_flat, lr_flat)
-
-
-# hr_lr_feats = F.interpolate(lr_feats, (h, w), mode="bilinear")
-
-# lr_flat: torch.Tensor = hr_lr_feats.reshape((c, -1)).permute((1, 0))
-# lr_hr_flat: torch.Tensor = hr_feats.reshape((c, -1)).permute((1, 0))
-
-# return (lr_hr_flat, lr_flat)
-
-
-class MLP(nn.Module):
+class Model(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int = 384):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(hidden_dim, input_dim),  # Output dimension matches input
+            nn.Conv2d(input_dim, hidden_dim, 1, 1),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
 
 
-def train_mlp(
+def train(
     train_data: torch.Tensor,
     target_data: torch.Tensor,
-    batch_size: int = 32,
     epochs: int = 100,
     lr: float = 1e-3,
     verbose=False,
     n_dims: int = 384,
     device: str = "cuda:0",
-) -> MLP:
-    model = MLP(n_dims).to(device)
+) -> Model:
+    _, c, _, _ = train_data.shape
+    model = Model(c, n_dims).to(device)
     criterion = nn.MSELoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-
-    dataset = TensorDataset(train_data, target_data)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     model.train()
     for epoch in range(epochs):
         total_loss = 0.0
-        for batch_x, batch_y in dataloader:
-            batch_x = batch_x.to(device)
-            batch_y = batch_y.to(device)
-
-            optimizer.zero_grad()
-            output = model(batch_x)
-            loss = criterion(output, batch_y)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+        optimizer.zero_grad()
+        output = model(train_data)
+        output_lr = F.avg_pool2d(output, (14, 14), 14)
+        loss = criterion(output_lr, target_data)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
 
         if verbose:
             print(f"Epoch {epoch + 1}/{epochs} - Loss: {total_loss:.4f}")
@@ -95,41 +51,23 @@ def train_mlp(
 
 
 @torch.no_grad()
-def apply(mlp: MLP, hr_feats: torch.Tensor) -> torch.Tensor:
-    _, c, h, w = hr_feats.shape
-    hr_flat = hr_feats.reshape((c, -1)).permute((1, 0))
-    res = mlp.forward(hr_flat)
-    res_2D = res.reshape((1, h, w, c)).permute((0, 3, 1, 2))
+def apply(mlp: Model, hr_feats: torch.Tensor) -> torch.Tensor:
+    res_2D = mlp.forward(hr_feats)
     return res_2D
 
 
 def vis(
-    save_path: str,
-    img: Image.Image | None,
-    lr_feats: torch.Tensor,
-    hr_feats: torch.Tensor,
-    weights: Mapping[str, Any] | None,
-    mlp: MLP | None = None,
+    save_path: str, img: Image.Image | None, lr_feats: torch.Tensor, hr_feats: torch.Tensor, remapped: torch.Tensor
 ):
-    _, c, h_, _ = hr_feats.shape
-
-    if mlp is None:
-        assert weights
-        mlp = MLP(c, 384)
-        mlp.load_state_dict(weights)
-        mlp = mlp.to(hr_feats.device)
-
-    res_2D = apply(mlp, hr_feats)
-
     lr_feats_np = lr_feats.cpu()[0].numpy()
     hr_feats_np = hr_feats.cpu()[0].numpy()
-    res_2D_np = res_2D.detach().cpu()[0].numpy()
+    res_2D_np = remapped.detach().cpu()[0].numpy()
 
     lr_feats_red = do_2D_pca(lr_feats_np, 3, post_norm="minmax")
     hr_feats_red = do_2D_pca(hr_feats_np, 3, post_norm="minmax")
     res_feats_red = do_2D_pca(res_2D_np, 3, post_norm="minmax")
 
-    fig, axs = plt.subplots(ncols=4)
+    _, axs = plt.subplots(ncols=4)
     axs[0].imshow(img)
     axs[1].imshow(lr_feats_red)
     axs[2].imshow(hr_feats_red)
@@ -141,76 +79,56 @@ def vis(
     plt.savefig(save_path, bbox_inches="tight")
 
 
-if __name__ == "__main__":
-    PATH = "data/imagenet_reduced"
-    DATA_FOLDER = "data_lu_reg"
-
-    fname = "00245"
-
-    img = Image.open(f"{PATH}/imgs/{fname}.png")
-    data = torch.load(f"{PATH}/{DATA_FOLDER}/{fname}.pt")
-    lr_feats: torch.Tensor = data["lr_feats"]
-    hr_feats: torch.Tensor = data["hr_feats"]
-
-    train, targ = get_train_data(lr_feats, hr_feats)
-
-    mlp = train_mlp(train, targ, 32, 1000, lr=1e-3, verbose=True)
-
-    res = apply(mlp, hr_feats)
-
-    vis("tmp/better_remap.png", img, lr_feats, hr_feats, None, mlp)
-
-
 # if __name__ == "__main__":
 #     PATH = "data/imagenet_reduced"
 #     DATA_FOLDER = "data_lu_reg"
-#     fnames = sorted(listdir(f"{PATH}/{DATA_FOLDER}"))
-#     N = len(fnames)
 
-#     for i, fname in enumerate(fnames):
-#         img = Image.open(f"{PATH}/imgs/{fname.split('.')[0]}.png")
-#         data = torch.load(f"{PATH}/{DATA_FOLDER}/{fname}", weights_only=True, map_location=DEVICE)
+#     fname = "00235"
 
-#         lr_feats: torch.Tensor = data["lr_feats"]
-#         hr_feats: torch.Tensor = data["hr_feats"]
+#     DEVICE = "cuda:1"
 
-#         train, targ = get_train_data(lr_feats, hr_feats)
-#         mlp = train_mlp(train, targ, batch_size=512, epochs=1000, lr=1e-3)
+#     featurizer_class = "dinov2s_reg"
+#     torch_hub_name = "loftup_dinov2s_reg"
 
-#         data["mlp_weights"] = mlp.state_dict()
-#         torch.save(data, f"{PATH}/{DATA_FOLDER}/{fname}")
+#     model, patch_size, dim = get_featurizer(featurizer_class)
+#     model = model.to(DEVICE)
 
-#         if i % 10 == 0:
-#             print(f"[{i:03d} / {N}]")
+#     kernel_size = patch_size
+#     lr_size = 224 // patch_size  # 2 * 224 // patch_size
+#     load_size = 224
 
+#     upsampler = torch.hub.load("andrehuang/loftup", torch_hub_name, pretrained=True)
+#     upsampler = upsampler.to(DEVICE).eval()
 
-# print(f"{end - start}s ")
-# print(mlp)
+#     denoiser = Denoiser(feat_dim=384).to(DEVICE)
+#     denoiser_weights = torch.load("yoeo/comparisons/vit_small_patch14_reg4_dinov2.lvd142m.pth")
+#     denoiser.load_state_dict(denoiser_weights["denoiser"])
 
-# print(res.shape)
-# torch.save(mlp, "tmp/mlp.pt")
-# VIS
+#     img = Image.open(f"{PATH}/imgs/{fname}.png")
+#     data = torch.load(f"{PATH}/{DATA_FOLDER}/{fname}.pt")
 
-# res = mlp.forward(hr_flat)
-# res_2D = res.reshape((1, h, w, c)).permute((0, 3, 1, 2))
+#     transform = T.Compose(
+#         [
+#             T.Resize(load_size, T.InterpolationMode.BILINEAR),
+#             T.CenterCrop(load_size),  # Depending on whether you want a center crop
+#             T.ToTensor(),
+#             norm,
+#         ]
+#     )
 
+#     normalized_img_tensor = transform(img).unsqueeze(0).to(DEVICE)
+#     with torch.no_grad():
+#         lr_feats = model(normalized_img_tensor)
+#         # lr_feats = lr_feats.permute((0, 2, 3, 1))
+#         # lr_feats = denoiser.forward(lr_feats, return_channel_first=True)
+#         hr_feats = upsampler(lr_feats, normalized_img_tensor)  # 1, dim, 224, 224
+#     # lr_feats: torch.Tensor = data["lr_feats"].to(DEVICE)
+#     # hr_feats: torch.Tensor = data["hr_feats"].to(DEVICE)
 
-# lr_feats_np = lr_feats.cpu()[0].numpy()
-# hr_feats_np = hr_feats.cpu()[0].numpy()
-# res_2D_np = res_2D.detach().cpu()[0].numpy()
+#     mlp = train(hr_feats, lr_feats, 2000, lr=1e-3, verbose=True, device=DEVICE)
 
-# lr_feats_red = do_2D_pca(lr_feats_np, 3, post_norm="minmax")
-# hr_feats_red = do_2D_pca(hr_feats_np, 3, post_norm="minmax")
-# res_feats_red = do_2D_pca(res_2D_np, 3, post_norm="minmax")
+#     res = apply(mlp, hr_feats)
+#     # print(torch.sum(mlp.model.weight.data))
+#     print(res.shape)
 
-
-# fig, axs = plt.subplots(ncols=4)
-# axs[0].imshow(img)
-# axs[1].imshow(lr_feats_red)
-# axs[2].imshow(hr_feats_red)
-# axs[3].imshow(res_feats_red)
-
-# for ax in axs:
-#     ax.set_axis_off()
-
-# plt.savefig("tmp/compare_pca_2_layers.png", bbox_inches="tight")
+#     vis("tmp/better_better_remap.png", img, lr_feats, hr_feats, res)
