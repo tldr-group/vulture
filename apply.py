68e79db4eedc24503132df949c0b037e1e6f289e
rmdocherty <rmdocherty2@gmail.com>
2025-06-05 07:57:00 +0100

    minor changes w new models

--- apply.py
+++ apply.py
@@ -1,37 +1,107 @@
 import torch
+import torch.nn.functional as F
+import torchvision.transforms.functional as TF
 from PIL import Image
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
-from yoeo.utils import to_numpy
+import numpy as np
+
+from yoeo.utils import to_numpy, do_2D_pca, closest_crop, convert_image
 from yoeo.main import get_dv2_model, get_upsampler_and_expr, get_hr_feats
+from yoeo.feature_prep import get_lr_feats, project
+from yoeo.comparisons.online_denoiser import Denoiser
+from yoeo.datasets.learn_remap_LU_feats import vis
 
 torch.backends.cudnn.enabled = True
 torch.cuda.empty_cache()
 
 DEVICE = "cuda:0"
 
-dv2 = get_dv2_model(True, device=DEVICE)
+dv2 = get_dv2_model(False, to_half=True, add_flash=True, device=DEVICE)
+# dv2 = get_dv2_model(True, to_half=False, add_flash=False, device=DEVICE)
+dv2 = dv2.to(DEVICE)
 
-model_path = "trained_models/e5000_fit_reg_f64.pth"
-cfg_path = "yoeo/models/configs/upsampler_fewer_features.json"
+denoiser = Denoiser(feat_dim=384).to(DEVICE)
+denoiser_weights = torch.load("yoeo/comparisons/vit_small_patch14_reg4_dinov2.lvd142m.pth")
+denoiser.load_state_dict(denoiser_weights["denoiser"])
+
+# model_path = "experiments/old/280525_up_lu_fixed_feats_half/best.pth"
+model_path = "trained_models/e180_full_dv2.pth"
+cfg_path = "yoeo/models/configs/upsampler_full_dv2.json"
+# model_path = "trained_models/e5000_full_fit_reg.pth"
+# cfg_path = "yoeo/models/configs/combined_no_shift.json"
+
+
+# model_path = "trained_models/e5000_fit_reg_f64.pth"
+# cfg_path = "yoeo/models/configs/upsampler_fewer_features.json"
+
 upsampler, expr = get_upsampler_and_expr(model_path, cfg_path, device=DEVICE)
 
-path = "data/apply/nmc.png"
+path = "data/compare/c3.jpg"
 img = Image.open(path).convert("RGB")
+# img = img.resize((img.width // 2, img.height // 2))
 
-hr_feats = get_hr_feats(img, dv2, upsampler, DEVICE, n_ch_in=expr.n_ch_in)
+tr = closest_crop(img.height, img.width, 14, True)
 
-hr_feats_np = to_numpy(hr_feats)
-reduced_hr = hr_feats_np[:3]
-c, h, w = reduced_hr.shape
-reduced_hr_flat = reduced_hr.reshape((c, h * w)).T
-reduced_hr_rescaled = MinMaxScaler(clip=True).fit_transform(reduced_hr_flat)
-reduced_hr = reduced_hr_rescaled.reshape((h, w, c))
-plt.imsave(
-    "test.png",
-    reduced_hr,
+
+inp_img = (
+    TF.normalize(
+        TF.pil_to_tensor(img).to(torch.float32),
+        [0.485, 0.456, 0.406],
+        [0.229, 0.224, 0.225],
+    )
+    .unsqueeze(0)
+    .to(DEVICE)
 )
+inp_img_dino = convert_image(img, tr, to_half=False, device_str=DEVICE)
+print(inp_img_dino.shape)
+
+with torch.autocast(DEVICE, torch.float16):
+    with torch.no_grad():
+        lr_feats = project(inp_img_dino, dv2, fit3d=False)
+        # lr_feats = dv2.forward_features(inp_img)["x_norm_patchtokens"]
+        lr_feats = lr_feats.permute((0, 2, 3, 1))
+        lr_feats = denoiser.forward(lr_feats, return_channel_first=True)
+        lr_feats = F.normalize(lr_feats, p=1, dim=1)
+
+        hr_feats = upsampler(inp_img, lr_feats)
+
+vis("tmp/test.png", img, lr_feats, hr_feats, None, True)
+
+
+# W = 1500
+# _w, _h = img.size
+# sf = _w / 448
+# new_h = int(_h / sf)
+# print(new_h)
+
+# img = img.resize((448, new_h))
+# oy = (new_h - 448) / 2
+# img = img.crop((0, oy, 448, oy + 448))
+
+# hr_feats = get_hr_feats(img, dv2, upsampler, DEVICE, fit_3d=False, n_ch_in=expr.n_ch_in)
+
+
+# reduced_tensor, _ = get_lr_feats(dv2, [inp_img_dino], 50, fit3d=True, n_feats_in=64)
+# reduced_tensor = reduced_tensor.to(DEVICE)
+# reduced_tensor = F.normalize(reduced_tensor, p=1, dim=1)
+
+# with torch.autocast("cuda", torch.float16):
+#     hr_feats = upsampler(inp_img, reduced_tensor)
+
+# hr_feats_np = to_numpy(hr_feats)
+# reduced_hr = do_2D_pca(hr_feats_np, 3).transpose((2, 0, 1))
+# # reduced_hr = hr_feats_np[:3]
+# c, h, w = reduced_hr.shape
+# print(reduced_hr.shape)
+# reduced_hr_flat = reduced_hr.reshape((c, h * w)).T
+# reduced_hr_rescaled = MinMaxScaler(clip=True).fit_transform(reduced_hr_flat)
+# reduced_hr = reduced_hr_rescaled.reshape((h, w, c))
+# plt.imsave(
+#     "tmp/test.png",
+#     reduced_hr,
+# )
 
 
 # # dv2 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14_reg")
