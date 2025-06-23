6934c7a3adb04d378a01ec3b15b95d8190c685a6
rmdocherty <rmdocherty2@gmail.com>
2025-06-23 22:06:07 +0100

    updated intro column to not usescale bar. still need to add new seg

--- apply.py
+++ apply.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 import numpy as np
+from time import time_ns
 
 from yoeo.utils import to_numpy, do_2D_pca, closest_crop, convert_image
 from yoeo.main import get_dv2_model, get_upsampler_and_expr, get_hr_feats
@@ -18,7 +19,7 @@ torch.cuda.empty_cache()
 
 DEVICE = "cuda:0"
 
-dv2 = get_dv2_model(False, to_half=True, add_flash=True, device=DEVICE)
+dv2 = get_dv2_model(True, to_half=False, add_flash=False, device=DEVICE)
 # dv2 = get_dv2_model(True, to_half=False, add_flash=False, device=DEVICE)
 dv2 = dv2.to(DEVICE)
 
@@ -27,18 +28,22 @@ denoiser_weights = torch.load("yoeo/comparisons/vit_small_patch14_reg4_dinov2.lv
 denoiser.load_state_dict(denoiser_weights["denoiser"])
 
 # model_path = "experiments/old/280525_up_lu_fixed_feats_half/best.pth"
-model_path = "trained_models/e180_full_dv2.pth"
-cfg_path = "yoeo/models/configs/upsampler_full_dv2.json"
-# model_path = "trained_models/e5000_full_fit_reg.pth"
-# cfg_path = "yoeo/models/configs/combined_no_shift.json"
-
+# model_path = "trained_models/e180_full_dv2.pth"
+# cfg_path = "yoeo/models/configs/upsampler_full_dv2.json"
+model_path = "trained_models/e5000_full_fit_reg.pth"
+cfg_path = "yoeo/models/configs/combined_no_shift.json"
 
 # model_path = "trained_models/e5000_fit_reg_f64.pth"
 # cfg_path = "yoeo/models/configs/upsampler_fewer_features.json"
 
-upsampler, expr = get_upsampler_and_expr(model_path, cfg_path, device=DEVICE)
+# model_path = "experiments/current/best.pth"
+# # model_path = "trained_models/e256_full_dv2.pth"
+# cfg_path = "yoeo/models/configs/upsampler_LU_narrow.json"
 
-path = "data/compare/c3.jpg"
+upsampler, expr = get_upsampler_and_expr(model_path, cfg_path, device=DEVICE)
+upsampler = upsampler
+
+path = "data/compare/bar.JPEG"
 img = Image.open(path).convert("RGB")
 # img = img.resize((img.width // 2, img.height // 2))
 
@@ -55,17 +60,46 @@ inp_img = (
     .to(DEVICE)
 )
 inp_img_dino = convert_image(img, tr, to_half=False, device_str=DEVICE)
-print(inp_img_dino.shape)
+# print(inp_img_dino.shape)
 
-with torch.autocast(DEVICE, torch.float16):
-    with torch.no_grad():
-        lr_feats = project(inp_img_dino, dv2, fit3d=False)
-        # lr_feats = dv2.forward_features(inp_img)["x_norm_patchtokens"]
-        lr_feats = lr_feats.permute((0, 2, 3, 1))
-        lr_feats = denoiser.forward(lr_feats, return_channel_first=True)
-        lr_feats = F.normalize(lr_feats, p=1, dim=1)
+torch.cuda.reset_peak_memory_stats(DEVICE)  # s.t memory is accurate
+torch.cuda.synchronize(DEVICE)  # s.t time is accurate
 
+
+def _to_MB(x: int) -> float:
+    return x / (1024**2)
+
+
+def _to_s(t: int) -> float:
+    return t / 1e9
+
+
+torch.cuda.reset_peak_memory_stats(DEVICE)  # s.t memory is accurate
+torch.cuda.synchronize(DEVICE)  # s.t time is accurate
+
+m0 = torch.cuda.max_memory_allocated(DEVICE)
+t0 = time_ns()
+
+with torch.no_grad():
+    # lr_feats = project(inp_img_dino, dv2, fit3d=False)
+    lr_feats, _ = get_lr_feats(dv2, [inp_img_dino], 50, fit3d=True, n_feats_in=expr.n_ch_in)
+    lr_feats = lr_feats.to(DEVICE)
+    # lr_feats = F.normalize(lr_feats, p=1, dim=1)
+
+    # lr_feats = lr_feats.permute((0, 2, 3, 1))
+    # lr_feats = denoiser.forward(lr_feats, return_channel_first=True)
+
+    lr_feats = F.normalize(lr_feats, p=1, dim=1)
+    lr_feats = lr_feats.to(torch.float16)
+    inp_img = inp_img.to(torch.float16)
+    with torch.autocast(DEVICE, torch.float16):
         hr_feats = upsampler(inp_img, lr_feats)
+
+torch.cuda.synchronize(DEVICE)  # s.t time is accurate
+t1 = time_ns()
+m1 = torch.cuda.max_memory_allocated(DEVICE)
+print(f"{hr_feats.shape} in {_to_s(t1 - t0):.3f}s and {_to_MB(m1 - m0):.0f}MB {hr_feats.dtype}")
+
 
 vis("tmp/test.png", img, lr_feats, hr_feats, None, True)
 
